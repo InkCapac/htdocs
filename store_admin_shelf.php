78f801f82e4d07e63f6d17a029cb514c92ea458e
Matheus_Bond <pachamamajunior@gmail.com>
2024-10-07 12:50:32 +0200

    CAMBIOS 07-10
    :)

--- store_admin_shelf.php
+++ store_admin_shelf.php
@@ -7,12 +7,13 @@ $bbdd = "diseno";
 if (isset($_POST["tipo"])) {
     $conexion = new Conectar($servidor, $usuario, $contrasena, $bbdd);
     $id_item = 1;
+    $contenedor = $_POST["tipo"];
     $nombre_item = $_POST["nombre_item"];
     $descripcion_item = $_POST["descripcion_item"];
     $precio_item = $_POST["precio_item"];
     $imagen_item = $_POST["imagen_item"];
 
-    $conexion->hacer_consulta("INSERT INTO catalogo (id_item, nombre_item, descripcion_item, precio_item, imagen_item) VALUES (?,?,?,?,?,?)", "iissis", [$id_item, $nombre_item, $descripcion_item, $precio_item, $imagen_item]);
+    $conexion->hacer_consulta("INSERT INTO catalogo (id_item, nombre_item, descripcion_item, precio_item, imagen_item, contenedor) VALUES (?,?,?,?,?,?)", "issisi", [$id_item, $nombre_item, $descripcion_item, $precio_item, $imagen_item, $contenedor]);
 }
 $conexion = new Conectar($servidor, $usuario, $contrasena, $bbdd);
 
@@ -34,6 +35,7 @@ $datos = $conexion->recibir_datos("SELECT * FROM catalogo ORDER BY id_item");
     <div class="grid grid-25-75">
         <h1>Datos del producto</h1>
         <form action="./store_admin_shelf.php" method="post">
+            <!--
             <p>Id del item<span class="star">*</span></p>
             <select name="id_item">
                 <option>1</option>
@@ -43,14 +45,21 @@ $datos = $conexion->recibir_datos("SELECT * FROM catalogo ORDER BY id_item");
                 <option>5</option>
                 <option>6</option>
             </select>
+            -->
             <p>Nombre Item<span class="star">*</span></p>
             <input type="text" name="nombre_item" id="">
             <p>Descripción del item<span class="star">*</span></p>
-            <textarea name="texto" cols="30" rows="10"></textarea>
+            <textarea name="descripcion_item" cols="30" rows="10"></textarea>
             <p>Precio del item</p>
             <input type="number" name="precio_item">
             <p>Imagen del item</p>
             <input type="text" name="imagen_item">
+            <p>Tipo</p>
+            <select name="tipo">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                </select>
             <button class="boton">Siguiente</button>
         </form>
         <?php
@@ -58,12 +67,13 @@ $datos = $conexion->recibir_datos("SELECT * FROM catalogo ORDER BY id_item");
             //Mostrar datos de la tabla
             foreach ($datos as $datos) {
                 $nombre_item = $datos["nombre_item"];
-                $descripcion_item = $datos["texto"];
+                $descripcion_item = $datos["descripcion_item"];
                 $precio_item = $datos["precio_item"];
                 $imagen_item = $datos["imagen_item"];
-                if ($id_item == 1) {
+                $contenedor = $datos["tipo"];
+                if ($contenedor == 1) {
                     echo '<h1>' . $nombre_item . '</h1><p>' . $descripcion_item . '</p><p>' . $precio_item . '</p><img src="' . $imagen_item . '" alt="' . $nombre_item . '">';
-                } elseif ($id_item == 2) {
+                } elseif ($contenedor == 2) {
                     echo "<h1>EL CONTENEDOR ES DE TIPO 2</h1>";
                 }
             }
