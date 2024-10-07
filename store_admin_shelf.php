94066e1aac499922697a1d4f509d1b7f5bdd950b
Matheus_Bond <pachamamajunior@gmail.com>
2024-10-07 12:32:39 +0200

    CAMBIOS 07-10
    :)

--- store_admin_shelf.php
+++ store_admin_shelf.php
@@ -14,6 +14,9 @@ if (isset($_POST["tipo"])) {
 
     $conexion->hacer_consulta("INSERT INTO catalogo (id_item, nombre_item, descripcion_item, precio_item, imagen_item) VALUES (?,?,?,?,?,?)", "iissis", [$id_item, $nombre_item, $descripcion_item, $precio_item, $imagen_item]);
 }
+$conexion = new Conectar($servidor, $usuario, $contrasena, $bbdd);
+
+$datos = $conexion->recibir_datos("SELECT * FROM catalogo ORDER BY id_item");
 ?>
 <!---->
 <!DOCTYPE html>
@@ -27,52 +30,47 @@ if (isset($_POST["tipo"])) {
 </head>
 
 <body>
-    <main class="cabecera">
-        <div class="grid grid-25-75">
-            <h1>Datos del producto</h1>
-            <form action="./store_admin_shelf.php" method="post"></form>
-            <div class="grid grid-50">
-                <p>Id del item<span class="star">*</span></p>
-                <select id="id_item">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                </select>
-                <p>Nombre Item<span class="star">*</span></p>
-                <input type="text" name="nombre_item" id="">
-                <p>Descripción del item<span class="star">*</span></p>
-                <textarea name="texto" id="" cols="30" rows="10"></textarea>
-                <p>Precio del item</p>
-                <input type="number" name="precio_item">
-                <p>Imagen del item</p>
-                <input type="text" name="imagen_item">
-            </div>
-        </div>
-        <div>
+    <!--<div class="cabecera">-->
+    <div class="grid grid-25-75">
+        <h1>Datos del producto</h1>
+        <form action="./store_admin_shelf.php" method="post">
+            <p>Id del item<span class="star">*</span></p>
+            <select name="id_item">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+            </select>
+            <p>Nombre Item<span class="star">*</span></p>
+            <input type="text" name="nombre_item" id="">
+            <p>Descripción del item<span class="star">*</span></p>
+            <textarea name="texto" cols="30" rows="10"></textarea>
+            <p>Precio del item</p>
+            <input type="number" name="precio_item">
+            <p>Imagen del item</p>
+            <input type="text" name="imagen_item">
             <button class="boton">Siguiente</button>
-        </div>
-    </main>
-    <?php
-    if ($datos) {
-        //Mostrar datos de la tabla
-        foreach ($datos as $datos) {
-            $nombre_item = $datos["nombre_item"];
-            $descripcion_item = $datos["texto"];
-            $precio_item = $datos["precio_item"];
-            $imagen_item = $datos["imagen_item"];
-            if ($id_item == 1) {
-                echo '<h1>' . $nombre_item . '</h1><p>' . $descripcion_item . '</p><p>' . $precio_item . '</p><img src="' . $imagen_item . '" alt="' . $titulo . '">';
-            } elseif ($contenedor == 2) {
-                echo "<h1>EL CONTENEDOR ES DE TIPO 2</h1>";
+        </form>
+        <?php
+        if ($datos) {
+            //Mostrar datos de la tabla
+            foreach ($datos as $datos) {
+                $nombre_item = $datos["nombre_item"];
+                $descripcion_item = $datos["texto"];
+                $precio_item = $datos["precio_item"];
+                $imagen_item = $datos["imagen_item"];
+                if ($id_item == 1) {
+                    echo '<h1>' . $nombre_item . '</h1><p>' . $descripcion_item . '</p><p>' . $precio_item . '</p><img src="' . $imagen_item . '" alt="' . $nombre_item . '">';
+                } elseif ($id_item == 2) {
+                    echo "<h1>EL CONTENEDOR ES DE TIPO 2</h1>";
+                }
             }
+        } else {
+            echo '<p>No hay datos disponibles</p>';
         }
-    } else {
-        echo '<p>No hay datos disponibles</p>';
-    }
-    ?>
+        ?>
 </body>
 
 </html>
