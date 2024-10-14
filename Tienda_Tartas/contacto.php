c6232bbd9d7a48b7e14405b61b6dbe173f6d0db0
InkCapac <160121572+InkCapac@users.noreply.github.com>
2024-10-14 12:28:41 +0200

    :)

--- Tienda_Tartas/contacto.php
+++ Tienda_Tartas/contacto.php
@@ -10,28 +10,29 @@
 </head>
 
 <body>
-    <div class="form-box">
-        <form>
-            <p>Forma parte de</p>
-            <div class="grid inputs">
-                <label for="name">Nombre</label>
-                <input type="text" id="name" name="name">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email">
-                <label for="phone">Teléfono</label>
-                <input type="tel" id="phone" name="phone">
-            </div>
-            <p>Información adicional</p>
-            <textarea id="additional-info" name="additional_info" cols="30" rows="20"></textarea>
-            <div class="grid checkbox-message">
-                <input type="checkbox" id="consent" name="consent" class="arriba">
-                <label for="consent">Por favor, haz click en este botón para asegurarnos que estás de acuerdo con
-                    nuestras condiciones de servicio.</label>
-            </div>
-            <button type="submit">Enviar datos</button>
-        </form>
-    </div>
-
+    <main>
+        <div class="form-box">
+            <form>
+                <p>Forma parte de</p>
+                <div class="grid inputs">
+                    <label for="name">Nombre</label>
+                    <input type="text" id="name" name="name">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email">
+                    <label for="phone">Teléfono</label>
+                    <input type="tel" id="phone" name="phone">
+                </div>
+                <p>Información adicional</p>
+                <textarea id="additional-info" name="additional_info" cols="30" rows="20"></textarea>
+                <div class="grid checkbox-message">
+                    <input type="checkbox" id="consent" name="consent" class="arriba">
+                    <label for="consent">Por favor, haz click en este botón para asegurarnos que estás de acuerdo con
+                        nuestras condiciones de servicio.</label>
+                </div>
+                <button type="submit">Enviar datos</button>
+            </form>
+        </div>
+    </main>
     <footer>
         <p>© Zarif Kamiso 2024 All rights reserved</p>
     </footer>
