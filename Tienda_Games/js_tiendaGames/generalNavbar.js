cc1cb88b3d57a2f3c40550844f019a52e5b87efd
Matheus Bond <pachamamajunior@gmail.com>
2024-10-18 14:19:00 +0200

    CAMBIOS 18-10
    :)

--- Tienda_Games/js_tiendaGames/generalNavbar.js
+++ Tienda_Games/js_tiendaGames/generalNavbar.js
@@ -1,11 +1,11 @@
-window.onscroll = function() {
+window.onscroll = function () {
     const navbar = document.querySelector('.navbar');
-    const galleryHeight = document.querySelector('.gallery').offsetHeight;
+    const imagePlaceholder = document.querySelector('.image-placeholder');
 
-    // Add or remove the 'solid' class based on scroll position
-    if (window.scrollY > galleryHeight) {
+    // Agregar o quitar la clase 'solid' según la posición de desplazamiento
+    if (window.scrollY > imagePlaceholder.offsetHeight) {
         navbar.classList.add('solid');
     } else {
-        navbar.classList.remove('solid');
+        navbar.classList.remove('solid');   
     }
 };
