697d53fe2382229da0e24b78d42ce02ff48b299f
Matheus Bond <pachamamajunior@gmail.com>
2024-10-18 11:19:35 +0200

    CAMBIOS 18-10
    :)

---
+++ Tienda_Games/products.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GameRRoom | Productos</title>
+     <!-- Fuente de Google Fonts-->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
+    <!--Archivos .css-->
+    <link rel="stylesheet" href="./css_tiendaGames">
+    <link rel="stylesheet" href="./css_tiendaGames/general_navbar.css">
+</head>
+<body>
+<header>
+        <div class="navbar nav-font">
+            </a>
+            <a href="#">Inicio</a>
+            <a href="">Productos y accesorios</a>
+            <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Red_Ribbon_Army_Logo.png" alt="Temporary logo">
+            <a>Socios</a>
+            <a>Eventos</a>
+        </div>
+    </header>
+        <div >
+            <p class="products-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sunt facere officia laboriosam a nostrum illum animi quaerat ratione nemo delectus necessitatibus iusto accusantium amet rem mollitia, aut officiis deleniti!</p>
+        </div>
+        <section>
+            <div>
+                <p>
+                    <img src="https://m.media-amazon.com/images/I/81ivHwA2ZpL._AC_UF1000,1000_QL80_.jpg" alt="Game 1">
+                </p>
+            </div>
+        </section>
+</body>
+</html>
